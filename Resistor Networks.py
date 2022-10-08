6009ab50c5a093f00bcdb9d9e54437182fb934b4
Vernon-Lee <107008215+Vernon-Lee@users.noreply.github.com>
2022-10-08 11:53:39 +0200

    Add files via upload

--- Resistor Networks.py
+++ Resistor Networks.py
@@ -1,19 +1,21 @@
 def resist_function(ohm):
-    def resist(resistance):
+    def res(resistance):
         # if user uses brackets
         # function will accept that the resistance is in parallel
         if type(resistance) == list:
             # calculate sum for parallel resistance
-            return 1 / sum(1 / resist(parallel) for parallel in resistance)
+            return 1 / sum(1 / res(parallel) for parallel in resistance)
         # if user uses parentheses
         # function will accept that the resistance is in series
         if type(resistance) == tuple:
             # calculate sum for series resistance
-            return sum(resist(series) for series in resistance)
+            return sum(res(series) for series in resistance)
         return resistance
-
-    return str(round(resist(eval(ohm)))) + " ohm"
+    # return sum as a string and round to the nearest tenth
+    return str(round(res(eval(ohm)), 1)) + " ohm"
 
 
 # print function
 print(resist_function("[2,3,6]"))
+print(resist_function("(2,3,6)"))
+print(resist_function("(10, [20, 30])"))
